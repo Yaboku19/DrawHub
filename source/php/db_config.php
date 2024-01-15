be760073a11f3156eb37497ad4546957d6d60dd6
Riccardo Frascio <riccardo.frascio@studio.unibo.it>
2024-01-15 19:46:36 +0100

    niente

--- source/php/db_config.php
+++ source/php/db_config.php
@@ -1,6 +1,6 @@
 <?php
 require_once("../database/database.php");
 require_once("../utils/function.php");
-$dbh = new DatabaseHelper("localhost", "root", "", "drawhub", 3307);
+$dbh = new DatabaseHelper("localhost", "root", "", "drawhub", 3306);
 define("UPLOAD_DIR", "../img/");
 ?>
