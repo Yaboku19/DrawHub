cfeb61345c14d396bf1c1395f6c7655c99e67490
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2024-01-17 15:30:05 +0100

    aggiustato i commit

--- source/database/test.sql
+++ source/database/test.sql
@@ -4,9 +4,9 @@ VALUES ('marcorossi', 'marcorossi', 'Questa è la mia bio.', 'defaultImage.png',
 INSERT INTO drawHub.user (username, password, bio, urlProfilePicture, birthDate, email, name, surname)
 VALUES ('mariobalo', 'mariobalo', 'Descrizione 2.', 'defaultImage.png', '1985-05-15', 'utente2@example.com', 'mario', 'balotelli');
 
-INSERT INTO drawHub.post (user, postID, description, urlImage)
-VALUES ('mariobalo', 1, 'Descrizione del primo post.', 'prova.jpg');
+INSERT INTO drawHub.post (user, postID, description, urlImage, datepost)
+VALUES ('mariobalo', 1, 'Descrizione del primo post.', 'prova.jpg', '2024-01-17');
 
-INSERT INTO drawHub.post (user, postID, description, urlImage)
-VALUES ('utente1', 1, 'Descrizione del secondo post.', 'prova.jpg');
+INSERT INTO drawHub.post (user, postID, description, urlImage, datePost)
+VALUES ('marcorossi', 1, 'Descrizione del secondo post.', 'prova.jpg', '2024-01-17');
 
