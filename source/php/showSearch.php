a286c06834389bdfde7e48b12d5b94b0722b7e6e
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2024-01-17 12:17:06 +0100

    tolto user id

--- source/php/showSearch.php
+++ source/php/showSearch.php
@@ -4,7 +4,7 @@ require_once("db_config.php");
 $templateParams["title"] = "Search";
 $templateParams["homepage"] = "";
 $templateParams["notifications"] = "";
-//$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", "../js/settings.js");
+$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", "../js/search.js");
 
 require("../template/base.php");
 ?>
